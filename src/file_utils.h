889a1097a2d87a64ead35b529c2ee00cae52b999
fpanighini <fpanighini@itba.edu.ar>
2024-11-04 01:08:08 -0300

    Formatted extra file

--- src/file_utils.h
+++ src/file_utils.h
@@ -5,27 +5,27 @@
 
 /**
  * @brief Get size from open file, recovers previous position
- * 
+ *
  * @param[in] file open file
  * @return file size on success, -1L on failure
  */
-long get_file_size(FILE *file);
+long get_file_size(FILE* file);
 
 /**
  * @brief Get pointer to file extension, references filename
- * 
- * @param[in] filename 
+ *
+ * @param[in] filename
  * @return file extension string including '.', empty string if not found
  */
-char *get_file_extension(const char *filename);
+char* get_file_extension(const char* filename);
 
 /**
  * @brief Reads all bytes from a file
- * 
- * @param[in] filename 
+ *
+ * @param[in] filename
  * @param[out] size_out returns size of the buffer
  * @return allocated buffer containing all bytes from file, NULL on failure
  */
-void *read_file_data(const char *filename, long *size_out);
+void* read_file_data(const char* filename, long* size_out);
 
 #endif // FILE_UTILS_H
